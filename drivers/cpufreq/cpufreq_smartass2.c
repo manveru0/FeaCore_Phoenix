05a7cab9750c9664fdd3c0e32652d6fe30f73068
Michel Juillard <manveru0@gmail.com>
2011-12-05 02:05:11 +0100

    apply latest OC changes

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -43,7 +43,7 @@
 * towards the ideal frequency and slower after it has passed it. Similarly,
 * lowering the frequency towards the ideal frequency is faster than below it.
 */
-#define DEFAULT_AWAKE_IDEAL_FREQ 1017600
+#define DEFAULT_AWAKE_IDEAL_FREQ 1305600
 static unsigned int awake_ideal_freq;
 
 /*
